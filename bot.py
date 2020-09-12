971f6ce1aff24eed48729edd85af3d987546dffc
Yury <66883095+MefDot@users.noreply.github.com>
2020-09-12 13:42:31 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,33 @@ roleUser = discord.Member
 result = []
 
 
+
+print ("""
+<html>
+
+<head><title>Sample CGI Script</title></head>
+
+<body>
+
+  <h3> Sample CGI Script </h3>
+""" )
+
+
+print ("""
+
+  <p>Previous message: %s</p>
+
+  <p>form
+
+  <form method="post" action="index.cgi">
+    <p>message: <input type="text" name="message"/></p>
+  </form>
+
+</body>
+
+</html>
+""")
+
 @client.event
 async def on_ready():
     print('Бот зашел: {0.user}'.format(client))
